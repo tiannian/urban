0d9c4bc08e047ca6bb06c6de5a46a69cc51062e4
dtiannian <dtiannian@gmail.com>
2026-02-05 18:05:03 +0000

    Move sol! macro to contracts.rs in uniswapv3 client

--- clients/uniswapv3/src/lib.rs
+++ clients/uniswapv3/src/lib.rs
@@ -1,50 +1,12 @@
+mod contracts;
+
 use alloy::primitives::{Address, U256};
 use alloy::providers::DynProvider;
-use alloy::sol;
 use anyhow::Result;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 
-// Generate PositionManager contract interface using alloy sol! macro
-sol! {
-    // PositionManager contract interface
-    #[sol(rpc)]
-    interface IPositionManager {
-        function balanceOf(address owner) external view returns (uint256);
-        function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-        function positions(uint256 tokenId) external view returns (
-            uint96 nonce,
-            address operator,
-            address token0,
-            address token1,
-            uint24 fee,
-            int24 tickLower,
-            int24 tickUpper,
-            uint128 liquidity,
-            uint256 feeGrowthInside0LastX128,
-            uint256 feeGrowthInside1LastX128,
-            uint128 tokensOwed0,
-            uint128 tokensOwed1
-        );
-        function decreaseLiquidity(DecreaseLiquidityParams calldata params) external returns (uint256 amount0, uint256 amount1);
-        function collect(CollectParams calldata params) external returns (uint256 amount0, uint256 amount1);
-    }
-
-    struct DecreaseLiquidityParams {
-        uint256 tokenId;
-        uint128 liquidity;
-        uint256 amount0Min;
-        uint256 amount1Min;
-        uint256 deadline;
-    }
-
-    struct CollectParams {
-        uint256 tokenId;
-        address recipient;
-        uint128 amount0Max;
-        uint128 amount1Max;
-    }
-}
+use contracts::{CollectParams, DecreaseLiquidityParams, IPositionManager};
 
 /// Position data structure containing all relevant information for a position
 #[derive(Debug, Clone)]
